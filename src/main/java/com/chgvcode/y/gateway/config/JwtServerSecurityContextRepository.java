98de64e42e25fce3a0e461757e0067c9bb897e0d
Christian G <christiangv99@gmail.com>
2026-02-09 20:36:14 +0000

    Clean imports

--- src/main/java/com/chgvcode/y/gateway/config/JwtServerSecurityContextRepository.java
+++ src/main/java/com/chgvcode/y/gateway/config/JwtServerSecurityContextRepository.java
@@ -1,15 +1,14 @@
 package com.chgvcode.y.gateway.config;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+
 import reactor.core.publisher.Mono;
 
 @Component
